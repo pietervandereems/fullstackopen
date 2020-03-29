3601f66bc63d37a12be3ff3fc50382d35ef4df6a
Pieter van der Eems <p.vandereems@vgz.nl>
2020-03-29 14:42:04 +0200

    part1, 1.1 complete. Refactor to separate components

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Header = (props) => (
+  <h1>{props.course}</h1>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Content = (props) => (
+  <>
+    <p>
+      {props.content.part1} {props.content.exercises1}
+    </p>
+    <p>
+      {props.content.part2} {props.content.exercises2}
+    </p>
+    <p>
+      {props.content.part3} {props.content.exercises3}
+    </p>
+  </>
+);
+
+const Total = (props) => (
+  <p>Number of exercises {props.content.exercises1 + props.content.exercises2 + props.content.exercises3}</p>
+);
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+  const courseContent = {
+    part1,
+    exercises1,
+    part2,
+    exercises2,
+    part3,
+    exercises3
+  };
+
+  return (
+    <>
+      <Header course={course} />
+      <Content content={courseContent} />
+      <Total content={courseContent} />
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
