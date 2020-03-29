128cac5cb66dced68cdae6ff7dbd746e09c8eb58
Pieter van der Eems <p.vandereems@vgz.nl>
2020-03-29 15:07:58 +0200

    part1, 1.5 complete

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -25,27 +25,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <>
-      <Header course={course} />
-      <Content content={parts} />
-      <Total content={parts} />
+      <Header course={course.name} />
+      <Content content={course.parts} />
+      <Total content={course.parts} />
     </>
   );
 };
