8c39c848038abecc2f45ffafca46ce51bb949ac0
Pieter van der Eems <p.vandereems@vgz.nl>
2020-04-04 13:50:28 +0200

    part1, 1.7 + 1.8 complete

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,17 +5,25 @@ const Button = ({ text, onClick }) => (
   <button onClick={onClick}>{text}</button>
 );
 
-const Statistics = ({ good, neutral, bad }) => (
-  <section>
-    <h1>statistics</h1>
-    good {good} <br />
-    neutral {neutral} <br />
-    bad {bad}
-  </section>
-);
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total ? (good - bad) / total : 0;
+  const positive = total ? (good / total) * 100 : 0;
+  const positivePercentage = positive ? positive.toString() + '%' : '-';
+  return (
+    <section>
+      <h1>statistics</h1>
+      good {good} <br />
+      neutral {neutral} <br />
+      bad {bad} <br />
+      all {total} <br />
+      average {average} <br />
+      positive {positivePercentage}
+    </section>
+  );
+};
 
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
