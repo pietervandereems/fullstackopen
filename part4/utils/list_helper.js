3991d17bf64f519c2adb03149a34f4069c011d6d
Pieter van der Eems <p.vandereems@vgz.nl>
2020-04-15 21:03:57 +0200

    part4, 4.5 complete

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,11 +2,12 @@ const dummy = () => {
   return 1;
 };
 
-const totalLikes = (blogs = []) => {
-  return blogs.reduce((acc, blog) => acc + blog.likes, 0);
-};
+const totalLikes = (blogs = []) => blogs.reduce((acc, blog) => acc + blog.likes, 0);
+
+const favoriteBlog = (blogs = []) => blogs.reduce((favorite, blog) => blog.likes >= favorite.likes ? blog : favorite, { likes: -1 });
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
