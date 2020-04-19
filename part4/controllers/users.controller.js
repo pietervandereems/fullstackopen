5149e1d1026905e69d3e95c79a67c4bfa49fe735
Pieter van der Eems <github_pietervandereems@eemco.nl>
2020-04-19 13:53:20 +0200

    part4, 4.15 complete

---
+++ part4/controllers/users.controller.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user.model');
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users.map(user => user.toJSON()));
+});
+
+usersRouter.post('/', async ({ body: { username, name, password } }, response) => {
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.json(savedUser);
+});
+
+module.exports = usersRouter;
---
+++ part4/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  passwordHash: String
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  }
+});
+
+
+module.exports = mongoose.model('User', userSchema);
