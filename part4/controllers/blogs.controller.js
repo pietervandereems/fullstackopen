23c14900d08a0e89153275c728158dd5460c38e6
Pieter van der Eems <github_pietervandereems@eemco.nl>
2020-04-19 10:52:34 +0200

    part4, 4.13, add delete and getone api.

--- part4/controllers/blogs.controller.js
+++ part4/controllers/blogs.controller.js
@@ -1,17 +1,30 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blogs.model');
 
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs.map(blog => blog.toJSON()));
 });
 
+blogsRouter.get('/:id', async ({ params: { id } }, response) => {
+  const blogs = await Blog.find({ _id: id });
+
+  if (blogs.length === 1) {
+    return response.json(blogs[0].toJSON());
+  }
+
+  response.status(404).end();
+});
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body);
   const savedBlog = await blog.save();
   response.status(201).json(savedBlog.toJSON());
+});
+
+blogsRouter.delete('/:id', async ({ params: { id } }, response) => {
+  await Blog.findByIdAndRemove(id);
+  response.status(204).end();
 });
 
 module.exports = blogsRouter;
