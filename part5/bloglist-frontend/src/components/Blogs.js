4d59fb231c9db9c94c7a0f46d5380be946fb544a
Pieter van der Eems <github_pietervandereems@eemco.nl>
2020-04-26 10:53:07 +0200

    part5, 5.9 complete

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -45,7 +45,10 @@ const Blogs = ({ user, setUser, sendNotification }) => {
         <CreateBlogs saveBlog={saveBlog} />
       </Togglable>
       <section>
-        {blogs.map(blog => <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />)}
+        {blogs
+          .sort((a, b) => b.likes - a.likes)
+          .map(blog => <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />)
+        }
       </section>
     </>
   );
