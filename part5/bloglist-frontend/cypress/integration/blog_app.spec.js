bb6e22295c61213397a33d1caae210842afc82b6
Pieter van der Eems <github_pietervandereems@eemco.nl>
2020-04-27 12:48:13 +0200

    part5, 5.17 complete

---
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -0,0 +1,14 @@
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset');
+    cy.visit('http://localhost:3000');
+    window.localStorage.setItem('user', '');
+  });
+
+  it('Login from is shown', function () {
+    cy.contains('log in to application');
+    cy.contains('username');
+    cy.contains('password');
+    cy.contains('login');
+  });
+});
