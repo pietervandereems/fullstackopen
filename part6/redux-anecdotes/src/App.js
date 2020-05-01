b62a691ffe837f217c33cd7897a6edb6bd2eea0a
Pieter van der Eems <github_pietervandereems@eemco.nl>
2020-05-01 19:17:05 +0200

    part6, 6.13 complete

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AnecdoteList from './components/AnecdoteList';
 import AnecdoteForm from './components/AnecdoteForm';
 import Notification from './components/Notification';
 import Filter from './components/Filter';
+import anecdoteService from './services/anecdotes.service';
+import { useDispatch } from 'react-redux';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then(notes => dispatch(initializeAnecdotes(notes)));
+  }, [dispatch]);
 
   return (
     <>
