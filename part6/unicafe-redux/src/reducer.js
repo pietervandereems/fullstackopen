e2cee4fcef66bd18db00207e7c046b1682ab4057
Pieter van der Eems <github_pietervandereems@eemco.nl>
2020-05-01 11:03:07 +0200

    part6, 6.1 complete

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -5,7 +5,7 @@ const initialState = {
 };
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action);
+  // console.log(action);
   switch (action.type) {
     case 'GOOD':
       return {
@@ -13,11 +13,21 @@ const counterReducer = (state = initialState, action) => {
         good: state.good + 1
       };
     case 'OK':
-      return state;
+      return {
+        ...state,
+        ok: state.ok + 1
+      };
     case 'BAD':
-      return state;
+      return {
+        ...state,
+        bad: state.bad + 1
+      };
     case 'ZERO':
-      return state;
+      return {
+        good: 0,
+        ok: 0,
+        bad: 0
+      };
     default: return state;
   }
 
