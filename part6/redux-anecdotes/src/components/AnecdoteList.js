6d2f6acb2796bb1d676eea476c989d5992cba6b7
Pieter van der Eems <github_pietervandereems@eemco.nl>
2020-05-04 13:12:06 +0200

    part6, 6.19 complete

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,36 +1,32 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { makeAVote } from '../reducers/anecdoteReducer';
-import { setNotification } from '../reducers/notificationReducer';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { makeAVote } from '../reducers/anecdoteReducer';
+// import { setNotification } from '../reducers/notificationReducer';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state.filter ?
-    state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase())) :
-    state.anecdotes
-  );
-
-  const dispatch = useDispatch();
-
-  const vote = (anecdote) => {
-    dispatch(setNotification(`you voted '${anecdote.content}'`, 10));
-    dispatch(makeAVote(anecdote));
-  };
-
-  return (
-    <>
-      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-        <article key={anecdote.id}>
-          <p>
-            {anecdote.content} <br />
+const AnecdoteList = ({ anecdotes }) => (
+  <>
+    {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+      <article key={anecdote.id}>
+        <p>
+          {anecdote.content} <br />
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote)}>vote</button>
-          </p>
-        </article>
-      )}
-    </>
-  );
+          <button onClick={() => true}>vote</button>
+        </p>
+      </article>
+    )}
+  </>
+);
 
+const mapStateToProps = ({ anecdotes, filter }) => ({
+  anecdotes: filter ?
+    anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase())) :
+    anecdotes
+});
+
+AnecdoteList.propTypes = {
+  anecdotes: PropTypes.array
 };
 
-
-export default AnecdoteList;
+export default connect(mapStateToProps)(AnecdoteList);
