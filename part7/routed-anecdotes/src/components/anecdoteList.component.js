e39e77c542be9a91dbd89b1ba6e92358e823a51b
Pieter van der Eems <github_pietervandereems@eemco.nl>
2020-05-06 11:00:24 +0200

    part7, split into components

---
+++ part7/routed-anecdotes/src/components/anecdoteList.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AnecdoteList = ({ anecdotes }) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <ul>
+      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+    </ul>
+  </div>
+);
+
+AnecdoteList.propTypes = {
+  anecdotes: PropTypes.array
+};
+
+export default AnecdoteList;
