e832fb0adb58357e5cc0c8f2c5ab194c0db1926a
Pieter van der Eems <github_pietervandereems@eemco.nl>
2020-05-16 16:21:14 +0200

    part8, 8.4 complete

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -107,7 +107,7 @@ const typeDefs = gql`
   type Query {
     bookCount : Int!,
     authorCount: Int!,
-    allBooks: [Book!]!,
+    allBooks(author:String): [Book!]!,
     allAuthors: [Author!]!
   }
 `;
@@ -116,7 +116,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => args.author ? books.filter(book => book.author === args.author) : books,
     allAuthors: () => authors.map(author => ({
       ...author,
       bookCount: books.filter(book => book.author === author.name).length
