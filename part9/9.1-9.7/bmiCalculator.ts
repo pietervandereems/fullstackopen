901e354668ba48f900197d98d8093c608c61659f
Pieter van der Eems <gpggit@eemco.nl>
2020-12-21 15:24:43 +0100

    Part 9, 9.1 , fix missing semicolons.

--- part9/9.1-9.7/bmiCalculator.ts
+++ part9/9.1-9.7/bmiCalculator.ts
@@ -1,4 +1,4 @@
-const getBMI = (height: number, weight: number): number => weight / ((height / 100) ** 2)
+const getBMI = (height: number, weight: number): number => weight / ((height / 100) ** 2);
 
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = getBMI(height, weight);
@@ -7,25 +7,25 @@ const calculateBmi = (height: number, weight: number): string => {
     return 'Very severely underweight';
   }
   if (bmi < 16) {
-    return 'Severely underweight'
+    return 'Severely underweight';
   }
   if (bmi < 18.5) {
-    return 'Underweight'
+    return 'Underweight';
   }
   if (bmi < 25) {
-    return 'Normal (healthy weight)'
+    return 'Normal (healthy weight)';
   }
   if (bmi < 30) {
-    return 'Overweight'
+    return 'Overweight';
   }
   if (bmi < 35) {
-    return 'Obese Class I (Moderately obese)'
+    return 'Obese Class I (Moderately obese)';
   }
   if (bmi < 40) {
-    return 'Obese Class II (Severely obese)'
+    return 'Obese Class II (Severely obese)';
   }
   
-  return 'Obese Class III (Very severely obese)'
+  return 'Obese Class III (Very severely obese)';
 }
 
-console.log(calculateBmi(180, 74))
+console.log(calculateBmi(180, 74));
